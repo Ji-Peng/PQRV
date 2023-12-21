554371ee4191a39d7053cb410295b7efe59574eb
Jipeng Zhang <jp-zhang@outlook.com>
2023-12-21 08:07:20 +0800

    in-place registers allocation from the paper Hybrid scalar/vector implementations of Keccak and SPHINCS+ on AArch64

--- Kyber/RV64/fips202.h
+++ Kyber/RV64/fips202.h
@@ -8,8 +8,9 @@
 #define SHAKE256_RATE 136
 #define SHA3_256_RATE 136
 #define SHA3_512_RATE 72
-// Supported ROUNDS_MERGING: 24, 12, 6, 3. The 24 rounds merging is the fastest and also will generate the largest executable file.
-#define ROUNDS_MERGING 24
+// Supported ROUNDS_MERGING: 24, 12, 6, 3, 1. The 24 rounds merging is the fastest
+// and also will generate the largest executable file.
+#define ROUNDS_MERGING 1
 
 /*************************************************
  * Name:        KeccakF1600_StatePermute
@@ -20,25 +21,28 @@
  **************************************************/
 #if ROUNDS_MERGING == 24
 extern void KeccakF1600_StatePermute_RV64_24r(uint64_t state[25]);
-#define KeccakF1600_StatePermute KeccakF1600_StatePermute_RV64_24r
+#    define KeccakF1600_StatePermute KeccakF1600_StatePermute_RV64_24r
 #elif ROUNDS_MERGING == 12
 extern void KeccakF1600_StatePermute_RV64_12r(uint64_t state[25]);
-#define KeccakF1600_StatePermute KeccakF1600_StatePermute_RV64_12r
+#    define KeccakF1600_StatePermute KeccakF1600_StatePermute_RV64_12r
 #elif ROUNDS_MERGING == 6
 extern void KeccakF1600_StatePermute_RV64_6r(uint64_t state[25]);
-#define KeccakF1600_StatePermute KeccakF1600_StatePermute_RV64_6r
+#    define KeccakF1600_StatePermute KeccakF1600_StatePermute_RV64_6r
 #elif ROUNDS_MERGING == 3
 extern void KeccakF1600_StatePermute_RV64_3r(uint64_t state[25]);
-#define KeccakF1600_StatePermute KeccakF1600_StatePermute_RV64_3r
+#    define KeccakF1600_StatePermute KeccakF1600_StatePermute_RV64_3r
+#elif ROUNDS_MERGING == 1
+extern void KeccakF1600_StatePermute_RV64_1r(uint64_t state[25]);
+#    define KeccakF1600_StatePermute KeccakF1600_StatePermute_RV64_1r
 #else
-#error "Unsupported ROUNDS_MERGING"
+#    error "Unsupported ROUNDS_MERGING"
 #endif
 
 #define FIPS202_NAMESPACE(s) pqcrystals_kyber_fips202_ref_##s
 
 typedef struct {
-  uint64_t s[25];
-  unsigned int pos;
+    uint64_t s[25];
+    unsigned int pos;
 } keccak_state;
 
 #define shake128_init FIPS202_NAMESPACE(shake128_init)
@@ -65,7 +69,7 @@ void shake256_squeeze(uint8_t *out, size_t outlen, keccak_state *state);
 #define shake256_absorb_once FIPS202_NAMESPACE(shake256_absorb_once)
 void shake256_absorb_once(keccak_state *state, const uint8_t *in, size_t inlen);
 #define shake256_squeezeblocks FIPS202_NAMESPACE(shake256_squeezeblocks)
-void shake256_squeezeblocks(uint8_t *out, size_t nblocks,  keccak_state *state);
+void shake256_squeezeblocks(uint8_t *out, size_t nblocks, keccak_state *state);
 
 #define shake128 FIPS202_NAMESPACE(shake128)
 void shake128(uint8_t *out, size_t outlen, const uint8_t *in, size_t inlen);
